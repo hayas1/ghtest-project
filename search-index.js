ba52eacf8a242638d93952fed051d8ec529ef8f2
hayas1 <hayas1@users.noreply.github.com>
2020-12-28 08:53:48 +0000

    deploy: c25ff105e24a8dc63a382ed1c52adaa218019edd

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"ghtest_project":{"doc":"","i":[],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["ghtest_project"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
